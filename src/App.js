3fcffe30c71d95ced34467699ac06127fe9c7734
gusgh <ghh357@navr.com>
2021-09-01 19:41:34 +0900

    연결확인

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import './App.css';
+
+document.title="nomad-react";
+
+const foodIlike = [
+  {img:"img"},
+  {name:"gusgh"}
+];
+
+function Food(props){
+  return <h1>I like {props.fav}</h1>
+}
+
+function reander(dish){
+  console.log(dish);
+  return <Food name="gusgh"/>
+}
 
 const App = () => {
   return (
     <>
-      <header>
-        
-      </header>
+      {foodIlike.map(reander)}      
     </>
   );
 };
