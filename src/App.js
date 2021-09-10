597ba22825b9eba573d59518b58a1c8efb59bb20
nrbns357 <gusgh357@gmail.com>
2021-09-10 19:25:45 +0900

    upDate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from "prop-types";
 
 document.title="nomad-react";
@@ -8,32 +8,37 @@ document.title="nomad-react";
 const App = () => {
 
 
-  const [state,setState] = useState(0);
-  const add = ()=>{
-    setState(state + 1);
-  };
-  const minus = () =>{
-    setState(state - 1);
-  };
-
-  componentDidMount() {
-    console.log("Component rendered");
-  }
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
-  }
+  const [isLoading,setIsLoading] = useState(true);
+  
+useEffect(()=>{
+  setIsLoading(false);
+}, 6000);
 
   return (
     <div>
-      <h1>The number is: {state}</h1>
-      <button onClick={add}>Add</button> 
-      <button onClick={minus}>Minus</button>
+      {isLoading ? "Loading..." : "We are ready"}
     </div>
   );
 };
 
+
+// class App extends React.Component{
+//   state ={
+//     isLoading: true
+//   };
+//   componentDidMount(){
+//     setTimeout(()=>{
+//       this.setState({isLoading: false});
+//     },6000);
+//   }
+//   render(){
+//     const{isLoading}=this.state;
+//     return(
+//       <>
+//         {isLoading? "Loading..." : "We are ready"}
+//       </>
+//     )
+//   }
+// }
 
 export default App;
