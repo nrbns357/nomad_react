83878adbdf4b28f688db1dbccf4ba3f7c967ffcc
nrbns357 <gusgh357@gmail.com>
2021-09-13 19:39:27 +0900

    upDate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from "axios";
+import Movie from "./Movie";
 
 document.title="nomad-react";
 
@@ -50,12 +51,14 @@ class App extends React.Component{
  
   render(){
      
-    const { isLoading } =this.state;
-    return(
-      <>
-        {isLoading? "Loading..." : "We are ready"}
-      </>
-    )
+    const { isLoading,movies } =this.state;
+    return <div>
+      {isLoading ? "Loading..." : movies.map(movie =>{
+        console.log(movie);
+        return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.poster}/>
+      })}</div>;
+
+    
   }
 }
 
