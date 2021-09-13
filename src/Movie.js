367ad031b1ea4755a9b9e71e364d0f05374b9341
nrbns357 <gusgh357@gmail.com>
2021-09-13 14:57:44 +0900

    upDate

---
+++ src/Movie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropType from "prop-types";
+
+const Movie = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Movie;
