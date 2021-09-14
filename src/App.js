102ba34c9cef0297ca7584b277d4b4755c4b64d1
nrbns357 <gusgh357@gmail.com>
2021-09-14 17:12:50 +0900

    upDate

--- src/App.js
+++ src/App.js
@@ -6,31 +6,6 @@ document.title="nomad-react";
 
 
 
-// const App = () => {
-
-
-//   const [isLoading,setIsLoading] = useState(true);
-  
-//   setTimeout (()=> 
-//   setIsLoading(false)
-//   , 5000);
-
-//     getMovies = async() =>{
-//       const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
-//     };
-
-//     componentDidMount() {
-//       getMovies();
-//     }
-
-//   return (
-//     <div>
-//       {isLoading ? "Loading..." : "We are ready"}
-//     </div>
-//   );
-// };
-
-
 class App extends React.Component{
   state ={
     isLoading: true,
